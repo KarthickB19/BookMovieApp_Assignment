a211a4da34bd9b98ff719e5c0f687a9c5afe4e21
Karthick B <danielkarthick499@gmail.com>
2023-02-26 00:39:18 +0530

    Added Button for Login, Logout, Book Show of material UI

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./Header.css";
 import logo from '../../assets/logo.svg';
 import { version } from 'react';
+import { Button } from "@material-ui/core";
 
 const Header = () => {
     return(
         <div className="header">
           <img src={logo} />
+          <Button variant="contained" className="float-right">Login</Button>
+          {/* <Button variant="contained" className="float-right">Logout</Button>
+          <Button variant="contained" color="primary" className="float-right">Book Show</Button> */}
         </div>
     )
 };
